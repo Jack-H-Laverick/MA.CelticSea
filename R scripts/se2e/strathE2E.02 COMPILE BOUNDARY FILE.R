f948a9a7aee7d890718564ad6be25dab9616e3f0
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-09-14 11:44:13 +0100

    feat: incorporate latest variables

--- R scripts/se2e/strathE2E.02 COMPILE BOUNDARY FILE.R
+++ R scripts/se2e/strathE2E.02 COMPILE BOUNDARY FILE.R
@@ -23,21 +23,50 @@ My_boundary_data<- readRDS("./Objects/Boundary measurements.rds") %>%           
 
 My_DIN_fix <- readRDS("./Objects/Ammonia to DIN.rds")
 
+My_atmosphere <- readRDS("./Objects/Atmospheric N deposition.rds") %>% 
+  filter(between(Year, 2003, 2013)) %>%                                                      # Limit to reference period
+  group_by(Month, Oxidation_state, Shore,  Year) %>%
+  summarise(Measured = sum(Measured, na.rm = T)) %>%                                         # Sum across deposition states 
+  summarise(Measured = mean(Measured, na.rm = T)) %>%                                        # Average over years
+  ungroup() %>% 
+  pivot_wider(names_from = c(Shore, Oxidation_state), values_from = Measured) %>%            # Spread to match template
+  arrange(Month)                                                                             # Order months ascending
+
 #### Create new file ####
 
 Boundary_new <- mutate(Boundary_template, 
                        SO_nitrate = My_boundary_data$SO_DIN * (1-filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion), # Multiply DIN by the proportion of total DIN as nitrate
-                       SO_ammonia = My_boundary_data$SO_DIN * filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion,     # Multiply DIN by the proportion of total DIN as ammonium
+                       SO_ammonia = My_boundary_data$SO_DIN * filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion, # Multiply DIN by the proportion of total DIN as ammonium
                        SO_phyt = My_boundary_data$SO_Phytoplankton,
                        SO_detritus = My_boundary_data$SO_Detritus,
-                       D_nitrate = My_boundary_data$D_DIN  * (1-filter(My_DIN_fix, Depth_layer == "Deep")$Proportion),     # Multiply DIN by the proportion of total DIN as nitrate
-                       D_ammonia = My_boundary_data$D_DIN * filter(My_DIN_fix, Depth_layer == "Deep")$Proportion,          # Multiply DIN by the proportion of total DIN as ammonium
+                       D_nitrate = My_boundary_data$D_DIN * (1-filter(My_DIN_fix, Depth_layer == "Deep")$Proportion), # Multiply DIN by the proportion of total DIN as nitrate
+                       D_ammonia = My_boundary_data$D_DIN * filter(My_DIN_fix, Depth_layer == "Deep")$Proportion, # Multiply DIN by the proportion of total DIN as ammonium
                        D_phyt = My_boundary_data$D_Phytoplankton,
                        D_detritus = My_boundary_data$D_Detritus,
-                       SI_nitrate = My_boundary_data$SI_DIN  * (1-filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion),# Multiply DIN by the proportion of total DIN as nitrate
-                       SI_ammonia = My_boundary_data$SI_DIN * filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion,     # Multiply DIN by the proportion of total DIN as ammonium
+                       SI_nitrate = My_boundary_data$SI_DIN * (1-filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion), # Multiply DIN by the proportion of total DIN as nitrate
+                       SI_ammonia = My_boundary_data$SI_DIN * filter(My_DIN_fix, Depth_layer == "Shallow")$Proportion, # Multiply DIN by the proportion of total DIN as ammonium
                        SI_phyt = My_boundary_data$SI_Phytoplankton, 
-                       SI_detritus = My_boundary_data$SI_Detritus) %>% 
+                       SI_detritus = My_boundary_data$SI_Detritus,
+                       ## Rivers
+                       # RIV_nitrate = My_river_N$Nitrate,     
+                       # RIV_ammonia = My_river_N$Ammonia,          
+                       # RIV_detritus = 0,
+                       ## Atmosphere, daily deposition as monthly averages
+                       SO_ATM_nitrate_flux = My_atmosphere$Offshore_O,
+                       SO_ATM_ammonia_flux = My_atmosphere$Offshore_R,
+                       SI_ATM_nitrate_flux = My_atmosphere$Inshore_O,
+                       SI_ATM_ammonia_flux = My_atmosphere$Inshore_R, 
+                       SI_other_nitrate_flux = 0,   # Can be used for scenarios
+                       SI_other_ammonia_flux = 0,
+                       SO_other_nitrate_flux = 0,   # Can be used for scenarios
+                       SO_other_ammonia_flux = 0,
+                       # ## Overhang
+                       # DO_nitrate = My_overhang$DIN *
+                       #   (Boundary_template$D_intrate/(Boundary_template$D_intrate + Boundary_template$D_ammonia)),    # Multiply DIN by the proportion of total DIN as nitrate
+                       # DO_ammonia	= My_overhang$DIN *
+                       #   (Boundary_template$D_ammonia/(Boundary_template$D_intrate + Boundary_template$D_ammonia)),
+                       # DO_detritus = My_overhang$Detritus
+) %>% 
   select(-c(SO_ammona, D_intrate))                                          # Fix Mike's typos
-                       
+
 write.csv(Boundary_new, file = "./StrathE2E/Celtic_Sea_NM/2003-2013/Driving/chemistry_CELTIC_SEA_2003-2013.csv", row.names = F)
--- R scripts/se2e/strathE2E.03 COMPILE PHYSICS FILE.R
+++ R scripts/se2e/strathE2E.03 COMPILE PHYSICS FILE.R
@@ -23,6 +23,13 @@ My_scale <- readRDS("./Objects/Domains.rds") %>%                            # Ca
   mutate(Volume = area * abs(Elevation)) %>% 
   dplyr::select(Shore, slab_layer, Volume)
 
+My_light <- readRDS("./Objects/Air temp and light.rds") %>% 
+  filter(between(Year, 2003, 2013), grepl("Light", Type)) %>%               # Limit to reference period and variable
+  group_by(Month) %>%                                                       # Average across months
+  summarise(Measured = mean(Measured, na.rm = T)) %>% 
+  ungroup() %>% 
+  arrange(Month)                                                            # Order to match template
+
 My_H_Flows <- readRDS("./Objects/H-Flows.rds") %>% 
   filter(between(Year, 2003, 2013)) %>%                                     # Limit to reference period
   group_by(across(-c(Year, Flow))) %>%                                      # Group over everything except year and variable of interest
@@ -33,7 +40,7 @@ My_H_Flows <- readRDS("./Objects/H-Flows.rds") %>%
   mutate(Flow = abs(Flow * 86400)) %>%                                      # Multiply for total daily from per second, and correct sign for "out" flows
   arrange(Month)                                                            # Order by month to match template
 
-My_V_Flows <- readRDS("./Objects/vertical diffusivity.rds") %>%
+My_V_Diff <- readRDS("./Objects/vertical diffusivity.rds") %>%
   filter(between(Year, 2003, 2013)) %>%                                     # Limit to reference period
   group_by(Month) %>% 
   summarise(V_diff = mean(Vertical_diffusivity, na.rm = T)) %>% 
@@ -52,20 +59,49 @@ My_volumes <- readRDS("./Objects/TS.rds") %>%
   ungroup() %>% 
   arrange(Month)                                                            # Order by month to match template
 
+My_Rivers <- readRDS("./Objects/River volume input.rds") %>% 
+  filter(between(Year, 2003, 2013)) %>%                                     # Limit to reference period
+  group_by(Month) %>% 
+  summarise(Runoff = mean(Runoff, na.rm = T)) %>%                           # Average by month across years
+  ungroup() %>% 
+  arrange(as.numeric(Month))                                                # Order by month to match template
+
 #### Create new file ####
 
-Physics_new <- mutate(Physics_template, ## Flows, should be proportions of volume per day
-                     SO_OceanIN = filter(My_H_Flows, slab_layer == "S", Shore == "Offshore", Neighbour == "Ocean", Direction == "In")$Flow,
-                     D_OceanIN = filter(My_H_Flows, slab_layer == "D", Shore == "Offshore", Neighbour == "Ocean", Direction == "In")$Flow,
-                     SI_OceanIN = filter(My_H_Flows, slab_layer == "S", Shore == "Inshore", Neighbour == "Ocean", Direction == "In")$Flow,
-                     SI_OceanOUT = filter(My_H_Flows, slab_layer == "S", Shore == "Inshore", Neighbour == "Ocean", Direction == "Out")$Flow,
-                     SO_SI_flow = filter(My_H_Flows, slab_layer == "S", Shore == "Offshore", Neighbour == "Inshore", Direction == "Out")$Flow,
-                     ## Temperatures in volumes for each zone
-                     SO_temp = filter(My_volumes, Compartment == "Offshore S")$Temperature_avg,
-                     D_temp = filter(My_volumes, Compartment == "Offshore D")$Temperature_avg,
-                     SI_temp = filter(My_volumes, Compartment == "Inshore S")$Temperature_avg ,
-                     ## Vertical diffusivity
-                     log10Kvert = log10(My_V_Flows$V_diff)) %>% 
-  mutate(log10Kvert = ifelse(log10Kvert == -Inf, 0, log10Kvert))
+Physics_new <- mutate(Physics_template, SLight = My_light$Measured,
+                      ## Flows, should be proportions of volume per day
+                      SO_OceanIN = filter(My_H_Flows, slab_layer == "S", Shore == "Offshore", Neighbour == "Ocean", Direction == "In")$Flow,
+                      D_OceanIN = filter(My_H_Flows, slab_layer == "D", Shore == "Offshore", Neighbour == "Ocean", Direction == "In")$Flow,
+                      SI_OceanIN = filter(My_H_Flows, slab_layer == "S", Shore == "Inshore", Neighbour == "Ocean", Direction == "In")$Flow,
+                      SI_OceanOUT = filter(My_H_Flows, slab_layer == "S", Shore == "Inshore", Neighbour == "Ocean", Direction == "Out")$Flow,
+                      SO_SI_flow = filter(My_H_Flows, slab_layer == "S", Shore == "Offshore", Neighbour == "Inshore", Direction == "Out")$Flow,
+                      # ## log e transformed suspended particulate matter concentration in zones
+                      # SO_LogeSPM = log(filter(My_SPM, Shore == "Offshore")$SPM),  
+                      # SI_LogeSPM = log(filter(My_SPM, Shore == "Inshore")$SPM),
+                      ## Temperatures in volumes for each zone
+                      SO_temp = filter(My_volumes, Compartment == "Offshore S")$Temperature_avg,
+                      D_temp = filter(My_volumes, Compartment == "Offshore D")$Temperature_avg,
+                      SI_temp = filter(My_volumes, Compartment == "Inshore S")$Temperature_avg ,
+                      ## River inflow,
+                      Rivervol_SI = My_Rivers$Runoff / filter(My_scale, Shore == "Inshore")$Volume, # Scale as proportion of inshore volume
+                      ## Vertical diffusivity
+                      log10Kvert = log10(My_V_Diff$V_diff),
+                      ## Daily proportion disturbed by natural bed shear stress
+                      # habS1_pdist = filter(My_Stress, Shore == "Inshore", Habitat == "Silt")$Disturbance, 
+                      # habS2_pdist = filter(My_Stress, Shore == "Inshore", Habitat == "Sand")$Disturbance,
+                      # habS3_pdist = filter(My_Stress, Shore == "Inshore", Habitat == "Gravel")$Disturbance,
+                      # habD1_pdist = filter(My_Stress, Shore == "Offshore", Habitat == "Silt")$Disturbance,
+                      # habD2_pdist = filter(My_Stress, Shore == "Offshore", Habitat == "Sand")$Disturbance,
+                      # habD3_pdist = filter(My_Stress, Shore == "Offshore", Habitat == "Gravel")$Disturbance, 
+                      # ## Monthly mean significant wave height inshore                     
+                      # Inshore_waveheight = My_Waves$Waves,
+                      # ## Overhang variables
+                      # D_SO_upwelling = My_V_Flows$Upwelling,
+                      # SO_D_downwelling = My_V_Flows$Downwelling, 
+                      # DO_log10Kvert = log10(My_overhang_diffusivity$V_diff),
+                      # DO_mixLscale = 0.9,
+                      # DO_D_upwelling = filter(My_overhang_velocity, Direction == "Upwelling")$Flow,
+                      # D_DO_downwelling = filter(My_overhang_velocity, Direction == "Downwelling")$Flow
+) 
                      
 write.csv(Physics_new, file = "./StrathE2E/Celtic_Sea_NM/2003-2013/Driving/physics_CELTIC_SEA_2003-2013.csv", row.names = F)
